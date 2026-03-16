420532083af048dce13ddfb435ce8b5e4c9467ec
twanofzo <twan.roodenburg@xs4all.nl>
2026-03-16 14:50:01 +0100

    .

--- build.sh
+++ build.sh
@@ -1,25 +1,2 @@
 #!/bin/bash
-
-# Recursively find all .cpp files in src directory
-SRC_DIR="src"
-CPP_FILES=$(find "$SRC_DIR" -type f -name '*.cpp')
-
-# Output executable name
-if [ ! -d "bin" ]; then
-    mkdir bin
-fi
-OUTPUT="bin/tmake"
-
-# Compiler and flags
-CXX=g++
-CXXFLAGS="-std=c++17 -Wall -Wextra -O2"
-
-# Compile all .cpp files
-$CXX $CXXFLAGS $CPP_FILES -o $OUTPUT
-
-if [ $? -eq 0 ]; then
-    echo "Build succeeded. Output: $OUTPUT"
-else
-    echo "Build failed."
-    exit 1
-fi
+g++ -o bin/tmake src/arguments.cpp src/builder.cpp src/executor.cpp src/lexer.cpp src/main.cpp src/parser.cpp -std=c++17 -Wall -Wextra -O2
